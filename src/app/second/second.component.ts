30475eb154a27f8fc0b923e8c12f39349b578960
Ajay-Gaikwad07 <130345085+Ajay-Gaikwad07@users.noreply.github.com>
2023-04-15 17:10:07 +0530

    Add files via upload

---
+++ src/app/second/second.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  templateUrl: './second.component.html',
+  styleUrls: ['./second.component.css']
+})
+export class SecondComponent {
+
+  constructor() { }
+public batsmen ="I wont 4runs to senchury"
+public fun= "Marvellous Infosystem" 
+
+  public data =""
+  public myaction()
+  {this.data="Educating for better tomorrow"}
+
+  public obj =""
+  public myaction2()
+  {this.obj="Marvellous Infosystem"}
+
+
+  public obj2 =""
+  public myaction3()
+  {this.obj2="Marvellous Infosystem"}
+}
